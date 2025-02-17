42883f7baa6bc1e3d3c9dbbb2b37979c09b71246
xia__mc <2052472631@qq.com>
2025-02-17 21:36:15 +0800

    编译工件名从clang_mc改为clang-mc 改进HelpMessage

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -9,7 +9,7 @@
 #include "config/ArgParser.h"
 #include "ClangMc.h"
 
-Config parseArgs(const int argc, const char *argv[]) {
+static inline Config parseArgs(const int argc, const char *argv[]) {
     auto config = Config();
     if (argc <= 1) {
         return config;
@@ -25,11 +25,23 @@ Config parseArgs(const int argc, const char *argv[]) {
     return config;
 }
 
+static inline std::string getExecutableName(const std::string& argv0) {
+    if (argv0.empty()) {  // 基本不可能发生，我想
+#ifdef _WIN32
+        return "clang-mc.exe";
+#else
+        return "clang-mc"
+#endif
+    }
+    return std::filesystem::path(argv0).filename().string();
+}
+
 int main(const int argc, const char *argv[]) {
+    assert(argc >= 1);
     if (argc == 2) {
         switch (hash(argv[1])) {
             case hash("--help"):
-                std::cout << HELP_MESSAGE << '\n';
+                std::cout << getHelpMessage(getExecutableName(argv[0])) << '\n';
                 return 0;
             case hash("--version"):
                 std::cout << fmt::format("{} version {}\n", ClangMc::NAME, ClangMc::VERSION);
