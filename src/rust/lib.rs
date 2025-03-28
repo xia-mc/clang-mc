ef065db677eb9a0c267a12384c57ac0bbe5a7723
xia__mc <2052472631@qq.com>
2025-03-28 17:56:55 +0800

    fast fix

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,7 +1,8 @@
 #![no_main]
 #![no_builtins]
 
-use std::panic::set_hook;
+use std::panic::{catch_unwind, set_hook};
+use std::process::abort;
 use crate::native::native::on_terminate;
 
 mod native;
@@ -12,6 +13,8 @@ mod parse;
 #[unsafe(no_mangle)]
 pub extern "C" fn ClRust_Init() {
     set_hook(Box::new(|_panic_info| {
-        on_terminate()
+        let _ = catch_unwind(|| {});
+        on_terminate();
+        abort();
     }));
 }
