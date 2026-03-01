3c6ce9c2e49df4b2c940e492720fd1ef6861ca47
xia__mc <2052472631@qq.com>
2026-03-01 23:14:52 +0800

    remove accidental files from previous commit

--- src/cpp/ir/ops/Calld.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by xia__mc on 2026/2/24.
-//
-
-#ifndef CLANG_MC_CALLD_H
-#define CLANG_MC_CALLD_H
-
-#endif //CLANG_MC_CALLD_H
--- src/cpp/ir/ops/Movd.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by xia__mc on 2026/2/24.
-//
-
-#ifndef CLANG_MC_MOVD_H
-#define CLANG_MC_MOVD_H
-
-#endif //CLANG_MC_MOVD_H
